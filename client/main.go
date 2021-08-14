4d167a30b96d7aa31085d2aebc8f837e3472e633
Ian Flynn <himitsu.monban@gmail.com>
2021-08-13 23:54:51 -0400

    use context.Context! yay!

--- client/main.go
+++ client/main.go
@@ -1,23 +1,61 @@
 package main
 
 import (
-  "nhooyr.io/websocket"
-  "nhooyr.io/websocket/wsjson"
-  "context"
-  "log"
+	"context"
+	"encoding/json"
+	"io"
+	"log"
+	"time"
+
+	"nhooyr.io/websocket"
 )
 
+const sleepTimeout = 3 * time.Second
+
+type Thing struct {
+	TimeStamp time.Time
+}
+
 func main() {
-  ctx := context.Background()
-  ws, _, err := websocket.Dial(ctx, "ws://localhost:8080", nil)
-  if err != nil {
-    log.Fatal(err)
-    return
-  }
-  var data interface{}
-  for {
-    if err := wsjson.Read(ctx, ws, &data); err != nil {
-      log.Fatal(err)
-    }
-  }
+	ctx := context.Background()
+	log.Println("Starting...")
+	run(ctx)
+}
+
+func run(ctx context.Context) {
+	for {
+		ws, _, err := websocket.Dial(ctx, "ws://localhost:8080", nil)
+		if err != nil {
+			log.Printf("Socket failed to connect: %v", err)
+			time.Sleep(sleepTimeout)
+			continue
+		}
+		log.Println("Socket connected")
+		if err := readStuff(ctx, ws); err != nil {
+			log.Println(err)
+		}
+		time.Sleep(sleepTimeout)
+	}
+}
+
+func readStuff(ctx context.Context, ws *websocket.Conn) error {
+	var thing Thing
+	for {
+		ctx_to, _ := context.WithTimeout(ctx, 5*time.Second)
+		mtype, r, err := ws.Reader(ctx_to)
+		if err != nil {
+			return err
+		}
+
+		data, err := io.ReadAll(r)
+		if err != nil {
+			return err
+		}
+
+		if err := json.Unmarshal(data, &thing); err != nil {
+			return err
+		}
+
+		log.Printf("%d %v\n", mtype, thing)
+	}
 }
--- srv/main.go
+++ srv/main.go
@@ -1,38 +1,75 @@
 package main
 
 import (
-  "net/http"
-  "nhooyr.io/websocket"
-  "time"
-  "log"
-  //"context"
+	"log"
+	"net/http"
+	"runtime"
+	"time"
+
+	"nhooyr.io/websocket"
+	"nhooyr.io/websocket/wsjson"
 )
 
 type wssrv struct {
 }
 
+type Thing struct {
+	TimeStamp time.Time
+}
+
 func (s wssrv) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  log.Printf("Upgrading %v to websocket", r.RemoteAddr)
-  ws, err := websocket.Accept(w, r, nil)
-  if err != nil {
-    log.Println(err.Error())
-  }
-  ws.CloseRead(r.Context())
+	log.Printf("Upgrading %v to websocket", r.RemoteAddr)
+	ws, err := websocket.Accept(w, r, nil)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	defer ws.Close(websocket.StatusNormalClosure, "goodbye")
+	ctx := ws.CloseRead(r.Context())
+	msgs := make(chan Thing)
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case msgs <- Thing{TimeStamp: time.Now()}:
+				time.Sleep(3 * time.Second)
+			}
+		}
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Printf("Closing connection: %v", ctx.Err())
+			return
+
+		case msg := <-msgs:
+			wsjson.Write(r.Context(), ws, msg)
+		}
+	}
 }
 
 func main() {
-  log.Printf("Starting...")
-  if err := run(); err != nil {
-    log.Fatalf(err.Error())
-  }
+	log.Printf("Starting...")
+	go debugData()
+	if err := run(); err != nil {
+		log.Fatalf(err.Error())
+	}
 }
 
 func run() error {
-  s := &http.Server{
-    Addr: ":8080",
-    Handler: wssrv{ },
-    ReadTimeout:  time.Second * 10,
-    WriteTimeout: time.Second * 10,
-  }
-  return s.ListenAndServe()
+	s := &http.Server{
+		Addr:         ":8080",
+		Handler:      wssrv{},
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 10,
+	}
+	return s.ListenAndServe()
+}
+
+func debugData() {
+	for {
+		log.Printf("Currently running goroutines: %v", runtime.NumGoroutine())
+		time.Sleep(5 * time.Second)
+	}
 }
